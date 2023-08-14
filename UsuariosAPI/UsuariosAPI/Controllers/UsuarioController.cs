a65e005a4d648ce9b7db974507b36ab4ff65fcf1
ricodealma <fael9339@gmail.com>
2023-08-14 11:51:14 -0300

    1.0.0

---
+++ UsuariosAPI/UsuariosAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.Dtos;
+using UsuariosApi.Services;
+
+namespace UsuariosApi.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class UsuarioController : ControllerBase
+    {
+
+        private UsuarioService _usuarioService;
+
+        public UsuarioController(UsuarioService cadastroService)
+        {
+            _usuarioService = cadastroService;
+        }
+
+
+        [HttpPost("cadastro")]
+        public async Task<IActionResult> CadastraUsuario
+            (CreateUsuarioDto dto)
+        {
+            await _usuarioService.CadastraUsuario(dto);
+            return Ok("Usuário cadastrado!");
+
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginAsync(LoginUsuarioDto dto)
+        {
+            var token = await _usuarioService.Login(dto);
+            return Ok(token);
+
+        }
+    }
+}
